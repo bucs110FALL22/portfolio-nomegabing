2a1d87f73043178f228ace76acbed6587ac65b08
Nicholas Genovese <112449708+nomegabing@users.noreply.github.com>
2022-09-06 21:46:11 +0000

    created part 2 of ch1's exercise "WORKING WITH DATA"

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -9,4 +9,20 @@ print(10 % 10)
 print(0 % 10)
 print(10 / 15) #the answer is neverending, but in the program it eventually ends
 
-var1 = 
+
+rate = int(input("Input ER from Euro to USD: "))
+#rate variable that asks for an exchange rate
+print("The Exchange Rate is ",[rate])
+#prints rate variable to user
+amount = int(input("How much currency to exchange: "))
+#input an amount to convert from user
+print("The input is $",[amount])
+#print the amount variable for user
+total = int(rate * amount)
+#creates total variable which is the converstion of the amount inputted and the rate
+print("USD Received before fees: ",[total])
+#prints the total variable for user
+result = int(total - 3)
+#creates result variable which subtracts the fee from total variable
+print("USD Received after $3 fee: ",[result])
+#prints the result variable for the user as a final result of the code
