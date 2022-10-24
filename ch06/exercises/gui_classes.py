d154c5486075d6fc2783a8c33ccdd782d5bdb9dd
Nicholas Genovese <112449708+nomegabing@users.noreply.github.com>
2022-10-24 21:58:02 +0000

    gui classes exercise completed

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,18 @@
+class Mario:
+  def __init__(self):
+    self.num_marios = 1
+    self.coins_collected = 0
+    self.position = (x, y)
+
+class Enemy:
+  def __init__(self):
+    self.num_enemies = 3
+    self.defeated = False
+    self.speed = 1
+
+class Pipe:
+  def __init__(self):
+    self.color = "Green"
+    self.entered = False
+    self.position = (x,y)
+
